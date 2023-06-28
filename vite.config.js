ce2256c2b2fa8003c75997bce793baa464e042e3
shubhhpatel <shubh.p013@gmail.com>
2023-06-28 18:31:43 -0400

    annoyingness

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/personalWeb/",
   build: {
     outDir: 'build', // Specify the output directory for the build
     sourcemap: true, // Enable sourcemaps for debugging (optional)
