afc1da96debe14f4f9c76b9d2bfc01c15c34db30
shubhhpatel <shubh.p013@gmail.com>
2023-06-28 18:27:22 -0400

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/personalWeb/",
   build: {
     outDir: 'build', // Specify the output directory for the build
     sourcemap: true, // Enable sourcemaps for debugging (optional)
