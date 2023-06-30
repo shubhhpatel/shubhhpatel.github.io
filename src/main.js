6a3a2f1c4dfc2c28d37a8e426c651126b465086a
Shubh Patel <shubh.p013@gmail.com>
2023-06-29 20:22:14 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import App from '/App.js';
+import App from 'App.js';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(React.createElement(
   React.StrictMode,
