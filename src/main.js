d9008d6ac2f233ec69d39e002bff2bc5320c5dc2
shubhhpatel <shubh.p013@gmail.com>
2023-07-01 17:54:22 -0400

    work

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const App = require('./App.js');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   React.createElement(
