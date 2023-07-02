4b781130ad2118663c3311acd37bb36f7ae580bf
shubhhpatel <shubh.p013@gmail.com>
2023-07-01 21:04:01 -0400

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/personalWebsite/',
-  plugins: [reactRefresh()]
+  plugins: [react()]
 });
